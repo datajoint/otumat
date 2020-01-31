a136cc5c0100dd989860c8cf0311af7335f12dc9
guzman-raphael <raphael.h.guzman@gmail.com>
2020-01-31 14:54:47 -0600

    Remove comments.

--- package.sh
+++ package.sh
@@ -3,6 +3,4 @@
 cd $1
 rm -R build dist *.egg-info
 python setup.py sdist bdist_wheel
-# python -m twine upload -s -i "DataJoint Dev" dist/*
 python -m twine upload dist/*
-# rm -R build dist *.egg-info
